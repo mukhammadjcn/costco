75af6d9dd48e39b3b93b9758e56d8a0e40761425
mukhammadjcn <mukhammadjcn@gmail.com>
2022-05-03 17:43:29 +0500

    Header created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from "./components/TheHeader.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { TheHeader },
+};
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style-type: none;
+  text-decoration-line: none;
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.container {
+  max-width: 1320px;
+  margin: 0 auto;
+}
+
+@media (max-width: 1140px) {
+  .container {
+    max-width: 90%;
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div class="container flex">
+      <img src="../assets/logoHeader.png" alt="" />
+      <p>We will update clearance product from costco everyday</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.header {
+  padding: 32px 0;
+  background: #dee1e3;
+
+  img {
+    height: 40px;
+  }
+
+  p {
+    font-weight: 600;
+  }
+}
+</style>
